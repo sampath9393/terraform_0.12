1d9af73ef19765f68c2f5e10982c9f7ed0474332
sampath9393 <91154651+sampath9393@users.noreply.github.com>
2021-09-29 19:34:10 -0700

    Update ec2instance.tf

--- demo/ec2instance.tf
+++ demo/ec2instance.tf
@@ -15,22 +15,6 @@ module "ec2_instance" {
   
 }
 
-module "ec2_instance2" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
-  name                   = "RAC-EC2_Second"
-  ami                    = var.ami
-  instance_type          = var.instance_type_map[var.env]
-  key_name               = var.instance_keypair
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = [module.instance_sg.this_security_group_id]
-  tags = merge(
-  local.common_tags,
-  {"Name" = "RAC_SQL_SERVER"
-   "env" = "var.env"}
-  )
-  
-}
 
 
 
