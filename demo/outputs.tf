4e5110ca723f8c2437e2921b69150f10d8189b5b
sampath9393 <91154651+sampath9393@users.noreply.github.com>
2021-09-29 23:36:25 -0700

    Update outputs.tf

--- demo/outputs.tf
+++ demo/outputs.tf
@@ -1,6 +1,16 @@
 
 
-output "httpd_public_ip" {
-  description = "List of public IP addresses assigned to the instances"
-  value       = module.ec2_instance.public_ip 
+###output "httpd_public_ip" {
+###  description = "List of public IP addresses assigned to the instances"
+###  value       = module.ec2_instance.public_ip 
+###}
+### The Ansible inventory file
+resource "local_file" "AnsibleInventory" {
+ description = "List of public IP addresses assigned to the instances"
+ content = templatefile("inventory.tmpl",
+ {
+  value       = module.ec2_instance.public_ip
+ }
+ )
+ filename = "inventory"
 }
