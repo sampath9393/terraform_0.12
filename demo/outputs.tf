2172ed76181f6816647473f9238d2d6ec2a0ad18
sampath9393 <91154651+sampath9393@users.noreply.github.com>
2021-09-29 20:01:25 -0700

    Update outputs.tf

--- demo/outputs.tf
+++ demo/outputs.tf
@@ -1,8 +1,4 @@
 
-output "sql_server_public_ip" {
-  description = "List of public IP addresses assigned to the instances"
-  value       = module.ec2_instance2.public_ip 
-}
 
 output "httpd_public_ip" {
   description = "List of public IP addresses assigned to the instances"
